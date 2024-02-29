fd97dc35c934ab1ce4aa96c003c293fa9531f8d8
Serhii Kononov <sergei97k@gmail.com>
2024-02-29 20:28:41 +0200

    added code samples for localStorage

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
 "use strict";
+
+// Зберігання значення
+// localStorage.setItem("username", "Ivan");
+
+// setTimeout(() => {
+//   localStorage.setItem("username", "Bob");
+// }, 5000);
+
+// Отримання значення
+// let username = localStorage.getItem("username");
+// console.log(username);
+
+// setTimeout(() => {
+//   localStorage.removeItem("username");
+// }, 5000);
+
+// let username = localStorage.getItem("username");
+// if (username) {
+//   console.log("username from storage", username);
+// } else {
+//   localStorage.setItem("username", "Bob");
+// }
+
+// localStorage.setItem("number", 5);
+
+// console.log(localStorage.getItem("number"));
+
+let user = {
+  name: "Bob",
+  email: "test@test.com",
+};
+
+localStorage.setItem("user", JSON.stringify(user));
+
+const storedUser = JSON.parse(localStorage.getItem("user"));
+console.log(storedUser);
+console.log(typeof storedUser);
