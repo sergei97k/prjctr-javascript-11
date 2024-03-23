d504b5b5004451fda3d77ccc4a5ef6f9b0a132eb
Serhii Kononov <sergei97k@gmail.com>
2024-03-23 16:29:18 +0200

    added DOM variables and form handler

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 "use strict";
+
+// DOM variables
+const bookForm = document.getElementById("book-form");
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const isbn = document.getElementById("isbn");
+const bookList = document.getElementById("book-list");
+
+// functions
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  const titleValue = title.value.trim();
+  const authorValue = author.value.trim();
+  const isbnValue = isbn.value.trim();
+
+  if (titleValue === "" || authorValue === "" || isbnValue === "") {
+    return;
+  }
+};
+
+// Event listeners
+bookForm.addEventListener("submit", handleSubmit);
